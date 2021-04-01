e404dfe693918aa2bb64d69c6c172c81bf63e762
sourabbapusridhar <sourabbsridhar@gmail.com>
2021-04-01 13:54:01 +0200

    Scripts Updated

--- scripts/pedestrian_action_benchmark.sh
+++ scripts/pedestrian_action_benchmark.sh
@@ -8,6 +8,7 @@
 BENCHMARK_REPO_PATH=/home/sourab/Data/test_folder/PedestrianActionBenchmark #e.g. /home/user/PedestrainActionBenchmark
 JAAD_REPO_PATH=/home/Datasets/MLDatasetsStorage/JAAD                        #e.g. /home/user/JAAD
 PIE_REPO_PATH=/home/Datasets/MLDatasetsStorage/PIE                          #e.g. /home/user/PIE
+BENCHMARK_VENV=Pedestrian_Action_Benchmark                                  #e.g. Pedestrian_Action_Benchmark
 SEQ_LEN=1                                                                   #e.g. 10,20,...
  
 #########################################################################################################
@@ -16,11 +17,19 @@ SEQ_LEN=1                                                                   #e.g
 
 # Cleanup (Temproary Step)
 
-# Print Provided Path
+# Print current path and provided path
+echo "Path of the bash script: $PWD"
 echo "Path to the Pedestrian Action Benchmark repository: $BENCHMARK_REPO_PATH"
 echo "Path to the JAAD repository: $JAAD_REPO_PATH"
 echo "Path to the PIE repository: $PIE_REPO_PATH"
 echo ""
+
+# Cleanup output folder
+echo "Cleaning up output folder..."
+echo ""
+OUTPUT_PATH="$(dirname "$PWD")/benchmark/$BENCHMARK_VENV"
+rm -rf $OUTPUT_PATH
+mkdir $OUTPUT_PATH
 
 # Clone the repository
 echo "Cloning the repository..."
--- scripts/social_stgcnn.sh
+++ scripts/social_stgcnn.sh
@@ -19,30 +19,31 @@ echo ""
 rm -rf ~/Data/test_folder/*
 conda remove --name $SOCIAL_STGCNN_VENV --all --yes
 
+# Cleanup output directory
+echo "Cleaning up output directory..."
+echo ""
+OUTPUT_PATH="$(dirname "$PWD")/benchmark/$SOCIAL_STGCNN_VENV"
+rm -rf $OUTPUT_PATH
+mkdir $OUTPUT_PATH
+
 # Print current path and provided path
 echo "Path of the bash script: $PWD"
 echo "Path to the Social-STGCNN repository: $SOCIAL_STGCNN_REPO"
+echo "Path to the output directory: $OUTPUT_PATH"
 echo ""
-
-# Cleanup Output Folder
-echo "Cleaning up output folder..."
-echo ""
-OUTPUT_PATH="$(dirname "PWD")/benchmark/$SOCIAL_STGCNN_VENV"
-rm -rf $OUTPUT_PATH
-mkdir $OUTPUT_PATH
 
 # Clone the repository
 echo "Cloning the repository..."
 echo ""
 git clone https://github.com/abduallahmohamed/Social-STGCNN.git $SOCIAL_STGCNN_REPO
 
-# Activate Conda Virtual Environment to run from Bash
-echo "Activating Conda Virtual Environment to run from Bash"
+# Activate conda virtual environment to run from Bash
+echo "Activating conda virtual environment to run from Bash"
 echo ""
 conda init bash
 
-# Create a Conda Virtual Environment
-echo "Creating a Conda Vritual Environment..."
+# Create a conda virtual environment
+echo "Creating a conda virtual environment..."
 echo ""
 conda create --yes --name $SOCIAL_STGCNN_VENV python=3.6
 conda activate $SOCIAL_STGCNN_VENV
@@ -55,7 +56,7 @@ pip install networkx
 pip install numpy
 pip install tqdm
 
-# Benchmark Model based on the pretrained weights and store initial results
+# Benchmark model based on the pretrained weights and store initial results
 echo "Benchmarking models based on pretrained weights and storing initial results..."
 echo ""
 cd  $SOCIAL_STGCNN_REPO
@@ -90,12 +91,13 @@ wait $P0 $P1 $P2 $P3 $P4
 
 python test.py |& tee test_${loop_counter}.txt
 
+mv -v $SOCIAL_STGCNN_REPO/test_${loop_counter}.txt  $SOCIAL_STGCNN_REPO/checkpoint/
 mv -v $SOCIAL_STGCNN_REPO/training_eth_${loop_counter}.txt $SOCIAL_STGCNN_REPO/checkpoint/social-stgcnn-eth/
 mv -v $SOCIAL_STGCNN_REPO/training_hotel_${loop_counter}.txt  $SOCIAL_STGCNN_REPO/checkpoint/social-stgcnn-hotel/
 mv -v $SOCIAL_STGCNN_REPO/training_univ_${loop_counter}.txt  $SOCIAL_STGCNN_REPO/checkpoint/social-stgcnn-univ/
 mv -v $SOCIAL_STGCNN_REPO/training_zara1_${loop_counter}.txt  $SOCIAL_STGCNN_REPO/checkpoint/social-stgcnn-zara1/
 mv -v $SOCIAL_STGCNN_REPO/training_zara2_${loop_counter}.txt  $SOCIAL_STGCNN_REPO/checkpoint/social-stgcnn-zara2/
-cp -rv $SOCIAL_STGCNN_REPO/checkpoint/* $OUTPUT_PATH
+cp -rv $SOCIAL_STGCNN_REPO/checkpoint/* $OUTPUT_PATH/Experiment_${loop_counter}
 
 done
 

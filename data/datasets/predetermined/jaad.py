bcc8bf8dd81776259060ef42e3e5d89e8d88df3a
Aren Moosakhanian <mooset_92@hotmail.com>
2021-05-17 14:37:23 +0200

    Correnctions
    jaad from .gitnore fixed 
    in config.json "true" changed to "True"

---
+++ data/datasets/predetermined/jaad.py
@@ -0,0 +1,64 @@
+# Implementation of dataset class for JAAD
+import torch
+import random
+import numpy as np
+from torch.utils.data import Dataset
+random.seed(0)
+
+class JAADDataset(Dataset):
+    """
+    Class implementation for JAAD Dataset. 
+    The class is inherited from nn.utils.data.Dataset
+    """
+    def __init__(self, split, isTrain, sequenceLength, imagePathFormat):
+        """
+        """
+        self.split = split
+        self.isTrain = isTrain
+        self.sequenceLength = sequenceLength
+        self.imagePathFormat = imagePathFormat
+
+    def __len__(self):
+        """
+        """
+        return len(self.pedstrians)
+
+    def __getitem__(self, idx, frameIdStart = -1):
+        """
+        """
+        pedestrian = self.pedestrian[idx]
+
+        if (frameIdStart != -1):
+            frameStart = frameIdStart
+        elif (self.isTrain) and (pedestrian['frame_start'] < pedestrian['frame_end'] - self.sequenceLength + 1):
+            frameStart = random.randint(pedestrian['frame_start'], pedestrian['frame_end'] - self.sequenceLength + 1)
+        else:
+            frameStart = self.pedestrian['frame_start']
+
+        if ((self.isTrain) or (frameIdStart != -1)):
+            frameIds = [min(pedestrian['frame_end'] - 1, frameStart + i) for i in range(self.sequenceLength)]
+        else:
+            frameIds = range(pedestrian['frame_start'], pedestrian['frame_end'])
+
+        gtAction = torch.tensor(np.stack([pedestrian['action'][frames] for frames in frameIds]))
+
+        gtBbox = torch.Tensor(np.stack(pedestrian['bbox'][frames].astype(np.float) if len(pedestrian['bbox'][frames]) else np.zeros(4) for frames in frameIds))
+        
+        objectClass = torch.tensor(np.stack([data['object_class'][frames] for frames in frameIds]))
+        
+        objectBbox = torch.tensor(np.stack([data['object_bbox'][frames] for frames in frameIds]))
+
+        frames = torch.tensor(np.array(frameIds))
+
+        imagePath = [self.imagePathFormat.format(pedestrian['video'], frames + 1) for frames in frameIds]
+
+        returnValue =   {
+                            'GT_Action' : gtAction, 
+                            'GT_Bbox' : gtBbox,
+                            'Object_Class' : objectClass,
+                            'Object_Bbox' : objectBbox,
+                            'Frame_Ids' : frames,
+                            'Image_Paths' : imagePath,
+                        }
+
+        return returnValue

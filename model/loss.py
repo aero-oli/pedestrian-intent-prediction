103e9166951f1497606360eb003ba08f1f4ada90
Aren Moosakhanian <mooset_92@hotmail.com>
2021-05-27 16:13:49 +0200

    loss update

--- model/loss.py
+++ model/loss.py
@@ -19,3 +19,23 @@ def negative_likelihood_loss(output, target):
               Negative Likelihood Loss
     """
     return function.nll_loss(output, target)
+
+
+
+def node_classification_loss(output, target):
+    """
+    Function to calculate the negative likelihood loss.
+
+    Parameters
+    ----------
+    output  : multiple
+              Output from the Neural Network
+    target  : multiple
+              Ground Truth Value
+
+    Returns
+    -------
+    loss    : float
+              Negative Likelihood Loss
+    """
+    return function.nll_loss(output, target)
